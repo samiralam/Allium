206518300fbeb6248eccbffb5a8ff9a1a23fd227
Wei Wen Goh <goweiwen@gmail.com>
2025-11-16 15:38:07 +0800

    fix: stop audioserver before ffplay starts
    Fixes an issue where ffplay could not access audio device if audioserver
    was running.

--- static/.allium/cores/ffplay/launch.sh
+++ static/.allium/cores/ffplay/launch.sh
@@ -24,6 +24,10 @@ set_snd_level() {
 
 set_snd_level &
 
+if [ -f /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh ]; then
+    /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+fi
+
 "$ROOT"/.allium/cores/ffplay/launch_ffplay.sh "$@"
 
 if [ -f /mnt/SDCARD/.tmp_update/script/start_audioserver.sh ]; then
